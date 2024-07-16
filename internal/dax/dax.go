34d807a623f62a6c403bd2e65e21ea83d3575db5
mloughton <matt.loughton@gmail.com>
2024-07-16 22:18:35 +0100

    updated generated dax and fixed clunky message box resets

--- internal/dax/dax.go
+++ internal/dax/dax.go
@@ -33,11 +33,12 @@ func ParseInput(input string) (*[]Column, error) {
 }
 
 func createColumn(raw string) (Column, error) {
-	badChars := ".,;':/\\*|?&%$!+=()[]{} <>"
+	badChars := ".,;':/\\*|?&%$!+=()[]{}<>"
 	validTypes := []string{"STRING", "INTEGER", "DATETIME"}
 	formatError := errors.New("incorrect format in input. expecting: table_name[column_name],type")
 	invalidTypeError := errors.New("invalid type in input. expecting : STRING, INTEGER, DATETIME")
 	badCharError := errors.New("invalid characters in input")
+	infError := errors.New("column/table name cannot begin with \"INF\"")
 	var col Column
 	tableColType := strings.Split(raw, ",")
 	if len(tableColType) != 2 {
@@ -54,18 +55,24 @@ func createColumn(raw string) (Column, error) {
 	}
 
 	table := []rune(tableCol[0])
+	if string(table[:3]) == "INF" {
+		return col, infError
+	}
 	if table[0] == '\'' && table[len(table)-1] == '\'' {
 		table = table[1 : len(table)-1]
-		if strings.ContainsAny(string(table), strings.ReplaceAll(badChars, " ", "")) {
-			return col, badCharError
-		}
-	} else {
 		if strings.ContainsAny(string(table), badChars) {
 			return col, badCharError
 		}
+	} else {
+		if strings.ContainsAny(string(table), badChars+" ") {
+			return col, badCharError
+		}
 	}
-	column := []rune(tableCol[1])
 
+	column := []rune(tableCol[1])
+	if string(column[:3]) == "INF" {
+		return col, infError
+	}
 	if column[len(column)-1] != ']' {
 		return col, formatError
 	}
@@ -85,28 +92,41 @@ func GenerateDax(cols *[]Column) (string, error) {
 	dax := ""
 	daxLink := ""
 	for i, col := range *cols {
+		urlTable := strings.ReplaceAll(col.tableName, " ", "_x0020_")
+		urlColumn := strings.ReplaceAll(col.columnName, " ", "_x0020_")
 		daxLink += fmt.Sprintf("V%d,", i)
 		dax += fmt.Sprintf(
 			`VAR V%d =
-	IF(
-		ISFILTERED( %s ) = TRUE(),
-		VAR vals = VALUES( %s )
-		VAR cnt = COUNTROWS(vals)
-		VAR valsStr = CONCATENATEX(vals, %s, "%%27,%%20%%27")
-		VAR fullStr = "%s/%s%%20" & IF(cnt = 1, "eq%%20%%27" & vals & "%%27", "in%%20(%%27" & valsStr & "%%27)")
-		VAR fullStrBlank = IF(valsStr = BLANK(), BLANK(), fullStr)
-		RETURN
-			fullStrBlank,
-		BLANK()
-	)
-`, i, col.fullName, col.fullName, col.fullName, strings.ReplaceAll(col.tableName, " ", "x0020"), strings.ReplaceAll(col.columnName, " ", "x0020"))
+    IF(
+        ISFILTERED( %s ) = TRUE(),
+        VAR vals = SELECTCOLUMNS(ADDCOLUMNS(VALUES( %s ), "clean",
+            SUBSTITUTE(SUBSTITUTE(SUBSTITUTE(SUBSTITUTE(SUBSTITUTE(SUBSTITUTE(SUBSTITUTE(SUBSTITUTE(%s,
+                " ", "%%20"),
+                "'", """"),
+                "%%", "%%25"),
+                "+", "%%2B"),
+                "/", "%%2F"),
+                "?", "%%3F"),
+                "#", "%%23"),
+                "&", "%%26")
+            ), "clean", [clean])
+        VAR cnt = COUNTROWS(vals)
+        VAR valsStr = IF(cnt = 1, "eq%%20%%27" & vals & "%%27", "in%%20(%%27" & CONCATENATEX(vals, [clean], "%%27,%%20%%27") & "%%27)")
+        VAR fullStr = "%s/%s%%20" & valsStr
+        VAR fullStrBlank = IF(valsStr = BLANK(), BLANK(), fullStr)
+    RETURN
+        fullStrBlank,   
+        BLANK()
+    )
+
+`, i, col.fullName, col.fullName, col.fullName, urlTable, urlColumn)
 	}
 	fullDax := fmt.Sprintf(
 		`%s
 VAR link = CONCATENATEX(FILTER({%s}, [Value] <> BLANK()), [Value], "%%20and%%20")
 VAR fullLink = IF(link = BLANK(), BLANK(), [Dashboard Link] & "?rs:embed=true&filter=" & link)
 RETURN
-fullLink
+	fullLink
 `, dax, string([]rune(daxLink)[:len(daxLink)-1]))
 	return fullDax, nil
 }
